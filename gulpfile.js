72943bd046be26379dda639a7c63aa7bb9c5d3be
ArmNo <monkeyarmno@gmail.com>
2014-02-10 19:47:31 +0700

    prepare basic gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
+var gulp = require('gulp');
 
+// run `cordova prepare` command
+gulp.task('prepare', function() {
+	console.log('preparing files');
+});
+
+gulp.task('default', function() {
+	// run cordova prepare when start gulp
+	gulp.run('prepare');
+	gulp.watch('www/**/*', function() {
+		gulp.run('prepare');
+	});
+});
